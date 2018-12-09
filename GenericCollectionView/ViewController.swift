4fa8d9fd59c731a3d084c62f9c6c1acf309b4ddd
m4riusz <sariuszm@gmail.com>
2018-12-09 16:19:17 +0100

    Initial Commit

---
+++ GenericCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericCollectionView
+//
+//  Created by Mariusz Sut on 09/12/2018.
+//  Copyright © 2018 Mariusz Sut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
